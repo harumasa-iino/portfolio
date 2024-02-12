f393b7e3426973d7a68bb039ebbb012779b957e7
Harumasa Iino <haru10masa18@gmail.com>
2024-02-12 22:26:26 +0900

    session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  before_action :ensure_session
+
+  private
+
+  def ensure_session
+    session[:session_id] ||= SecureRandom.hex(10) # セッションIDがなければ生成
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_240_211_015_217) do
+ActiveRecord::Schema[7.0].define(version: 20_240_212_125_356) do
   create_table 'rooms', charset: 'utf8mb4', force: :cascade do |t|
     t.string 'image'
     t.string 'image_cache'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'session_id'
   end
 end
