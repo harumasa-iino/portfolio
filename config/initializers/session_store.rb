cd5719c819f23a542ceca43342c1125e2bedfc25
Harumasa Iino <haru10masa18@gmail.com>
2024-02-13 08:29:11 +0900

    軽微な修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_user_id_session', expire_after: 1.month
+Rails.application.config.session_store :cookie_store, key: '_session_id', expire_after: 1.month
 # session期限を1ヶ月に設定
