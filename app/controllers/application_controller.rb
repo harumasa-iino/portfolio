704adaf9b05183243bd6fa782afbe3d0f62bab7b
Harumasa Iino <haru10masa18@gmail.com>
2024-02-13 23:43:10 +0900

    function_selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
     session[:session_id] ||= SecureRandom.hex(10) # セッションIDがなければ生成
   end
 
-  # Answeに追加
-#  def create
-#    @answer = Answer.new(answer_params)
-#    @answer.session_id = session[:session_id]  # セッションIDをAnswerレコードに保存
-#    if @answer.save
-#    else
-#    end
-#  end
+  # Answerに追加
+  #  def create
+  #    @answer = Answer.new(answer_params)
+  #    @answer.session_id = session[:session_id]  # セッションIDをAnswerレコードに保存
+  #    if @answer.save
+  #    else
+  #    end
+  #  end
 end
