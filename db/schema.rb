69916c618718b8fb98e967099e8fff248ea39044
Harumasa Iino <haru10masa18@gmail.com>
2024-02-14 23:49:15 +0900

    一時的な変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_240_212_125_356) do
-  create_table 'rooms', charset: 'utf8mb4', force: :cascade do |t|
-    t.string 'image'
-    t.string 'image_cache'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'session_id'
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_143843) do
+  create_table "answers", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "option"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
+
+  create_table "questions", charset: "utf8mb4", force: :cascade do |t|
+    t.text "body"
+    t.text "first_option"
+    t.text "second_option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
+    t.string "image"
+    t.string "image_cache"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "session_id"
+  end
+
+  add_foreign_key "answers", "questions"
 end
