0614694ad1799778cecfb4a55e0e5dfcd85ee5d3
Harumasa Iino <haru10masa18@gmail.com>
2024-03-06 23:25:46 +0900

    poster_show

--- app/controllers/composite_images_controller.rb
+++ app/controllers/composite_images_controller.rb
@@ -1,4 +1,5 @@
 class CompositeImagesController < ApplicationController
+
   def index
     # ここでパラメータから壁紙とポスターのパスを取得します
     session_id = session[:session_id]
@@ -15,5 +16,14 @@ class CompositeImagesController < ApplicationController
     CompositeImage.create_composite(room.id, wallpaper_path, poster_path)
 
     @composite_image = CompositeImage.where(room_id: room.id).order(id: :desc).first
+  end
+
+  def show
+    session_id = session[:session_id]
+    user_result = UserResult.where(session_id:).order(created_at: :desc).first
+
+    category_id = user_result.category_id
+    poster_results = PosterResult.where(category_id:)
+    @poster = Poster.where(id: poster_results.pluck(:poster_id)).first
   end
 end
