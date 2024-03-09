9721fd1eef13960d44aec8c217339d886ca7f0ce
Harumasa Iino <haru10masa18@gmail.com>
2024-03-09 15:20:35 +0900

    css_setup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,6 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
+# Rails.application.config.assets.precompile += %w[application.js]
 
 # a)で"bootstap.min.js"ではなく、"bootsrap.js"と書いた場合は、こちらも同じに直す
