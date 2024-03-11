ab7a7b92f641edfe67d31a3b0fb436567cfcbff2
Harumasa Iino <haru10masa18@gmail.com>
2024-03-12 00:40:12 +0900

    出典の設置

--- app/controllers/composite_images_controller.rb
+++ app/controllers/composite_images_controller.rb
@@ -1,29 +1,26 @@
 class CompositeImagesController < ApplicationController
+  before_action :set_room_and_poster, only: [:index]
 
   def index
-    # ここでパラメータから壁紙とポスターのパスを取得します
-    session_id = session[:session_id]
-    user_result = UserResult.where(session_id:).order(created_at: :desc).first
+    wallpaper_path = @room.image.path
+    poster_path = @poster.image.path
+    CompositeImage.create_composite(@room.id, wallpaper_path, poster_path)
 
-    category_id = user_result.category_id
-    poster_results = PosterResult.where(category_id:)
-    poster = Poster.where(id: poster_results.pluck(:poster_id)).first
-
-    room = Room.find_by(session_id:)
-
-    wallpaper_path = room.image.path
-    poster_path = poster.image.path
-    CompositeImage.create_composite(room.id, wallpaper_path, poster_path)
-
-    @composite_image = CompositeImage.where(room_id: room.id).order(id: :desc).first
+    @composite_image = CompositeImage.where(room_id: @room.id).order(id: :desc).first
   end
 
   def show
-    session_id = session[:session_id]
-    user_result = UserResult.where(session_id:).order(created_at: :desc).first
-
-    category_id = user_result.category_id
-    poster_results = PosterResult.where(category_id:)
-    @poster = Poster.where(id: poster_results.pluck(:poster_id)).first
+    set_room_and_poster
   end
 end
+
+  private
+
+  def set_room_and_poster
+    session_id = session[:session_id]
+    user_result = UserResult.where(session_id:).order(created_at: :desc).first
+    return unless user_result
+
+    @poster = Poster.joins(:poster_results).find_by(poster_results: {category_id: user_result.category_id})
+    @room = Room.find_by(session_id: session_id)
+  end
