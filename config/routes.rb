0e419ef04c3af91d458be2145b147d719ae04ee7
Harumasa Iino <haru10masa18@gmail.com>
2024-03-12 23:44:30 +0900

    deviseのinstall

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'home#index'
   resources :rooms, only: %i[new create show edit update destroy]
