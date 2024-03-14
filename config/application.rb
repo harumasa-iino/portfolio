ab97954dea3b38d148540ae0d63ee5b3965a488b
Harumasa Iino <haru10masa18@gmail.com>
2024-03-14 23:50:23 +0900

    login_css

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Portfolio
 
     config.assets.enabled = true
 
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
