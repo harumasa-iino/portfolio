36e55b69957ce2d899667ca83d961a25fe18fdcf
Harumasa Iino <haru10masa18@gmail.com>
2024-03-22 00:59:04 +0900

    button

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
+    @total_questions = Question.count
+    @current_question_number = Question.where('id <= ?', @question.id).count
   end
 
   def index
