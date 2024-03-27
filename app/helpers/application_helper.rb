f8c1cecdf1b1cc8733cd127e641ef096067fcee6
Harumasa Iino <haru10masa18@gmail.com>
2024-03-27 23:01:12 +0900

    SEO対策

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,30 @@ module ApplicationHelper
       'alert-info'
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'Select Frame',
+      og: defalut_og,
+#      twitter: default_twitter_card
+    }
+  end
+  
+  private
+  
+  def defalut_og
+    {
+      title: :full_title,
+      description: :description,
+      url: request.url,
+#      image: 'https://example.com/hoge.png'
+    }
+  end
+  
+#  def default_twitter_card
+#    {
+#      card: 'summary_large_image',
+#      site: '@hogehoge'            # twitter ID
+#    }
+#  end
 end
