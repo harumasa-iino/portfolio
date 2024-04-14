77823b6d63507daad6442a43c945a428a5f249b0
Harumasa Iino <haru10masa18@gmail.com>
2024-04-14 17:56:40 +0900

    座標参照し画像合成

--- app/models/composite_image.rb
+++ app/models/composite_image.rb
@@ -7,25 +7,26 @@ class CompositeImage < ApplicationRecord
 
   require 'mini_magick'
 
-  def self.create_composite(room_id, wallpaper_path, poster_path, poster_id, poster_width, poster_height)
+  def self.create_composite(room_id, wallpaper_path, poster_path, poster_id, poster_width, poster_height, x_percent, y_percent)
     wallpaper = MiniMagick::Image.open(wallpaper_path)
     poster = MiniMagick::Image.open(poster_path)
 
     poster.resize "#{poster_width}x#{poster_height}"
 
-    x_offset = (wallpaper.width - poster.width) / 2
-    y_offset = (wallpaper.height - poster.height) / 2
+    # パーセンテージからピクセル値に変換
+    x_offset = (wallpaper.width * x_percent.to_f / 100) - (poster.width / 2)
+    y_offset = (wallpaper.height * y_percent.to_f / 100) - (poster.height / 2)
 
     result = wallpaper.composite(poster) do |composite|
       composite.compose 'Over' # ポスターを壁紙の上に重ねる
-      composite.geometry "+#{x_offset}+#{y_offset}" # ポスターの位置を指定
+      composite.geometry "+#{x_offset.to_i}+#{y_offset.to_i}" # ポスターの位置をピクセル単位で指定
     end
 
     temp_file = Tempfile.new(['result', '.jpg'])
     result.write(temp_file.path)
 
     # CarrierWaveを使って加工した画像を保存
-    composite_image = new(room_id:, poster_id:)
+    composite_image = new(room_id: room_id, poster_id: poster_id)
     composite_image.composite_image = temp_file
     composite_image.save!
 
