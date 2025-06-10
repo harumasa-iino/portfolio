67c8f1207d7a98401bd82b4103b1a6c3b90da077
harumasa-iino <139933050+harumasa-iino@users.noreply.github.com>
2025-06-10 09:33:12 +0900

    Remove duplicate application pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,5 @@
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin 'application', to: 'controllers/application.js'
 pin 'hello_controller', to: 'controllers/hello_controller.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
